ce9db049aa085fd49a37fa2a9f9b3d13f65ae9f0
suraj <surajbanthot@gmail.com>
2020-08-21 08:03:28 +0530

    perfect number problem in c++

---
+++ perfect.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number, sum = 0;
+    cin >> number;
+    for (int i = 1; i < number; i++)
+        if (number % i == 0)
+            sum = sum + i;
+    if (sum == number)
+        cout <<sum<<" true\n";
+    else
+        cout <<sum<< " false\n";
+    return 0;
+}
