9e42bd274c4e8d9894d8e3cd0a4992ab2ae0024b
suraj <surajbanthot@gmail.com>
2020-08-21 05:59:10 +0530

    Toys and Boxes Problem

--- Boxes_toys.cpp
+++ Boxes_toys.cpp
@@ -1,4 +1,5 @@
-// Tina and Rahul have 1toy each. They are entering into an amusement park but it is not allowed to take the toys inside, so they have to keep it in the boxes provided by the park management. They want to keep the toys together in one box. There are  Nboxes in total, at this moment there are  t i toys present in  i t h box and the maximum capacity of the box is denoted by  c i. Rahul and Tina want to know in how many boxes can they keep their toys such that both the toys are in the same box.
+// Tina and Rahul have 1toy each. They are entering into an amusement park but it is not allowed to take the toys inside, so they have to keep it in the boxes provided by the park management. They want to keep the toys together in one box. There are  Nboxes in total, at this moment there are  t i toys present in  i t h box and the maximum capacity of the box is denoted by  c i. Rahul and Tina want to know in how many boxes can they keep their toys such that both the toys are in the same box. input are first testcases n and later
+// ti and ci where ti is the number of toys and ci is the capacity of that box. 
 #include <iostream>
 
 using namespace std;
