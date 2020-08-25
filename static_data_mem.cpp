c825a8afce6236c411f10e44aae47560884bd7de
suraj <surajbanthot@gmail.com>
2020-08-26 02:27:29 +0530

    static data members and member functions

---
+++ static_data_mem.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class Test
+{
+    public:
+    int a;
+    static int count;
+
+    Test()
+    {
+        a=10;
+        count++;
+    }
+
+    static int getCount()
+    {
+       return count; 
+    }
+};
+
+int Test::count=0;
+
+int main()
+{
+    Test t1, t2;
+    cout<<t1.count<<endl;
+    cout<<t2.count<<endl;
+    t1.count=25;
+    cout<<t2.count<<endl;
+    cout<<Test::count<<endl;
+
+    cout<<Test::getCount()<<endl;
+}
