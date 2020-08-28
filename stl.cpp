965f7b1201cdb97105df772cea2ab33a3e6deeba
suraj <surajbanthot@gmail.com>
2020-08-28 15:41:40 +0530

    STL: list

--- stl.cpp
+++ stl.cpp
@@ -1,24 +1,46 @@
 #include<iostream>
-#include<vector>
+// #include<vector>
+#include<list>
+
 
 using namespace std;
 
 int main()
 {
-    vector<int> v={2,4,6,8,10};
-    v.push_back(20);
-    v.push_back(30);
+    // vector<int> v={2,4,6,8,10};
+    // v.push_back(20);
+    // v.push_back(30);
 
-    // using for each loop
-    // for(int x:v)
-    // cout<<x<<endl;
+    // // using for each loop
+    // // for(int x:v)
+    // // cout<<x<<endl;
 
-    //use of iterator
-    vector<int>::iterator itr;
+    // //use of iterator
+    // vector<int>::iterator itr;
 
-    cout<<"using Iterator"<<endl;
-    for(itr=v.begin();itr!=v.end();itr++)
+    // cout<<"using Iterator"<<endl;
+    // for(itr=v.begin();itr!=v.end();itr++)
+    // cout<<*itr<<endl;
+
+
+    list<int> i={2,3,4,5,6};
+    i.push_front(10);
+    i.push_back(20);
+    i.pop_back();
+    i.pop_front();
+
+    list<int>::iterator itr;
+
+    cout<<"Using Iterator"<<endl;
+
+    for(itr=i.begin();itr!=i.end();itr++)
     cout<<*itr<<endl;
+
+    cout<<"using for each"<<endl;
+
+    for(int x:i)
+    cout<<x<<endl;
+    
 
 
     
